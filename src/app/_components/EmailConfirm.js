ceef5a3c063d1454adb9637d3ea25350f89cdec4
milo <milo.grv@gmail.com>
2025-07-15 19:22:21 +0200

    missing pages
    TODO: validations des formulaires, etc

--- src/app/_components/EmailConfirm.js
+++ src/app/_components/EmailConfirm.js
@@ -85,67 +85,41 @@ export default function LoginForm() {
     }
 
     return (
-        <Form onSubmit={handleSubmit} className="flex flex-col w-full gap-4 max-w-[340px] mt-6">
-            <div className="w-full items-start content-center flex flex-col gap-6 ">
+        <div className="flex flex-col w-full gap-4 font-[family-name:var(--font-plus-jakarta-sans)]">
+            <div className="w-full items-start content-center flex flex-col gap-6">
                 <div className="w-full">
-                    <p className="text-[var(--light-text)] text-[16px] font-thin ml-4">Adresse mail</p>
-                    <input
-                        type="email"
-                        className={inputsClass}
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+                    <p className="text-[16px] font-normal">Merci pour ton inscription !</p>
                 </div>
 
                 <div className="w-full">
-                    <p className="text-[var(--light-text)] text-[16px] font-thin ml-4">Mot de passe</p>
-                    <div className="relative">
-                        <input
-                            type={pswVisible ? "text" : "password"}
-                            className={inputsClass}
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <button
-                            type="button"
-                            onClick={() => setPswVisible(!pswVisible)}
-                            className="absolute right-3 h-full top-0 flex items-center text-[var(--light-text)] cursor-pointer"
-                        >
-                            {pswVisible ? <EyeOff size={18} /> : <Eye size={18} />}
-                        </button>
-                    </div>
-                    <p className="mt-3">
-                        <a href="#" className="text-[var(--light-text)] text-[14px] font-thin underline">Mot de passe oublié</a>
+                    <p className="text-[16px] font-normal">
+                        Nous t'avons envoyé un email de confirmation à l'adresse suivante :
+                        <br/>
+                        {'{adresse@mail.com}'}
+                        <br/>
+                        Clique sur le lien dans cet email pour activer ton compte.
                     </p>
                 </div>
             </div>
 
-            <div className="w-full items-start content-center flex flex-col gap-4">
-                <button
-                    className="text-white rounded-full flex items-center justify-center bg-[#4EA04C] font-medium text-[16px] h-[44px] w-full cursor-pointer "
-                    type="submit"
-                >
-                    Se connecter
-                </button>
-                <a
-                    href='/sign-up-particulier'
-                    className="text-[var(--primary)] rounded-full flex items-center justify-center border-2 border-[var(--primary)] font-medium text-[16px] h-[44px] w-full cursor-pointer "
-                >
-                    Créer un compte
-                </a>
+            <div className="w-full flex flex-col gap-5 mt-5 rounded-[10px] bg-[#F4F4F4] p-5">
+                <p className="text-[16px] font-normal flex gap-1">
+                    <Image src="/lockIcon.svg" alt="lockIcon" width={24} height={24} />
+                    Pas reçu l'email ?
+                </p>
+                <p className="text-[16px] font-normal">Vérifie ton dossier spam ou courrier indésirable.</p>
+                <p className="text-[16px] font-normal">ou <a href='#' className="underline text-[var(--primary)]">renvoyer l'email de confirmation</a></p>
             </div>
 
             <div className="flex w-full place-items-center gap-3 mt-4">
                 <hr className="flex-1/3 text-[#E3E3E3]"/>
-                <p className="text-[var(--light-text)] grow-1 shrink-0 font-[family-name:var(--font-plus-jakarta-sans)] font-[500] text-[16px]">ou se connecter avec</p>
+                <p className="text-[var(--light-text)] grow-1 shrink-0 font-[family-name:var(--font-plus-jakarta-sans)] font-[500] text-[16px]">Mauvaise adresse ?</p>
                 <hr className="flex-1/3 text-[#E3E3E3]"/>
             </div>
 
-            <div className="flex w-full place-content-center gap-3">
-                <button className="flex place-items-center gap-3 px-2 py-2 border-2 rounded-full border-[#E3E3E3]">
-                    <Image src="/google.png" alt={'Google'} width={30} height={30}/>
-                </button>
+            <div className="w-full mt-5">
+                <p className="text-[16px] font-normal">Tu peux changer ton adresse : <a href='/sign-up-producteur' className="underline text-[var(--primary)]">Modifier mon adresse email</a></p>
             </div>
-        </Form>
+        </div>
     )
 }
--- src/app/validation-email/page.js
+++ src/app/validation-email/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import LoginForm from "./_components/LoginForm";
+import EmailConfirm from "../_components/EmailConfirm";
 
 export default function Home(){
   return (
     <div className="flex items-center justify-center lg:justify-start 2xl:justify-center h-screen p-4 w-full sm:p-4 font-[family-name:var(--font-poppins)]">
-      <main className="bg-white flex flex-col items-center w-[600px] row-start-2 rounded-[16px] p-8 py-16 sm:px-8 overflow-y-auto max-h-full max-h-full no-scrollbar">
-        <div className="w-full items-center content-center flex flex-col gap-4 pb-8">
+      <main className="bg-white flex flex-col items-center w-[600px] row-start-2 rounded-[16px] p-[30px] py-15 sm:px-10 overflow-y-auto max-h-full no-scrollbar">
+        <div className="w-full items-center content-center flex flex-col gap-4 pb-10">
           <Image
               src="/logo_large.png"
               alt="Kouer logo"
@@ -13,10 +13,10 @@ export default function Home(){
               height={45}
               priority
           />
-          <p className="text-[#4EA04C] text-[20px] font-medium">Content de vous revoir !</p>
+          <p className="text-[#4EA04C] text-[20px] font-medium">Validez votre email !</p>
         </div>
 
-        <LoginForm />
+        <EmailConfirm />
       </main>
     </div>
   );
